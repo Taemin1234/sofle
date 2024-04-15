e8af559c960a09d70b311a028916bd4438411416
TM Book <cestmoi7531@gmail.com>
2024-04-15 19:19:06 +0900

    코드 주석 달기

--- src/chapter14/ThemeContext.js
+++ src/chapter14/ThemeContext.js
@@ -3,5 +3,6 @@ import React from "react";
 const ThemeContext = React.createContext();
 
 ThemeContext.displayName = 'ThemeContext'
+// 디버깅과 개발자도구에서 해당 컨텍스트의 이름을 알 수 있는 장치
 
 export default ThemeContext
